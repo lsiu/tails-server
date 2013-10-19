81ecc8fb20e2a36b3144c57a8e00c6e62f4a85af
lsiu <leonard.siu@gmail.com>
2013-10-19 14:24:10 +0800

    add rhc ip and port

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http");
 
+//Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}).listen(port, ipaddr);
